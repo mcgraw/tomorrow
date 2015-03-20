69ae3dd351ebbbc77b45d7c18546c3b99001240d
David McGraw <dlmcgraw07@gmail.com>
2015-03-19 20:17:18 -0500

    Bunch of polish, addition of the about screen

---
+++ Tomorrow/IGIAboutTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  IGIAboutTableViewController.swift
+//  Tomorrow
+//
+//  Created by David McGraw on 3/19/15.
+//  Copyright (c) 2015 David McGraw. All rights reserved.
+//
+
+import UIKit
+
+class IGIAboutTableViewController: UITableViewController {
+    
+}
